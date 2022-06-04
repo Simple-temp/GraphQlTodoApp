60846a425af648e03d46ac1b89036b5efebf2c58
Simple-temp <fiveriam01@gmail..com>
2022-06-04 13:08:53 +0600

    add graphql server

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <Link className="navbar-brand" to="/">T R I P L E N I N E </Link>
+                <Link className="navbar-brand" to="/profile">T R I P L E N I N E </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
