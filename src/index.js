58fe6debdd142b03a69224411e65fb4990544df3
Simple-temp <fiveriam01@gmail..com>
2022-06-04 17:46:34 +0600

    some line change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
   headers : {
     authorization : localStorage.getItem("token") || ""
